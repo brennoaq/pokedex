1239b32f06574f942cf6ce2d29ce14dacd350021
brenno araujo queiroz <brennoaraujoqueiroz@gmail.com>
2020-04-13 18:48:03 -0300

    implements structure + implements component appBar

---
+++ lib/consts/consts_app.dart
@@ -0,0 +1,7 @@
+class ConstsApp {
+
+  static const String colorPokeball = 'assets/images/pokeball_color.png';
+  static const String whitePokeball = 'assets/images/pokeball_white.png';
+  static const String blackPokeball = 'assets/images/pokeball_black.png';
+
+}
---
+++ lib/pages/home_page/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/consts/consts_app.dart';
+
+import 'widgets/app_bar_home.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double statusWidth = MediaQuery.of(context).padding.top;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        alignment: Alignment.topCenter,
+        overflow: Overflow.visible,
+        children: <Widget>[
+          Positioned(
+            top: -(240 / 4.7),
+            left: screenWidth - (240 / 1.6),
+            child: Opacity(
+              child: Image.asset(
+                ConstsApp.blackPokeball,
+                height: 240,
+                width: 240,
+              ),
+              opacity: 0.1,
+            ),
+          ),
+          Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: statusWidth,
+                ),
+                AppBarHome(),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
